fc7fe100e8dc0cd48f5bda374669ecb56ccb8513
Tom White <tom@sixdozen.com>
2018-10-04 12:42:40 +1300

    Starting code with zoomable interface

---
+++ drawgrid.js
@@ -0,0 +1,30 @@
+/*
+ * This is the function to implement to make your own abstract design.
+ *
+ * arguments:
+ * p5: the p5.js object - all draw commands should be prefixed with this object
+ * x1, x2, y1, y2: draw the pattern contained in the rectangle x1,y1 to x2, y2
+ * z: use this as the noise z offset (can be shifted)
+ * zoom: current zoom level (starts at 0), useful to decide how much detail to draw
+ *
+ * The destination drawing should be in the square 0, 0, 255, 255.
+ */
+
+
+/* the random number seed for the tour */
+var tourSeed = 301;
+/* triplets of locations: zoom, x, y */
+var tourPath = [
+  [2, 512, 512],
+  [4, 512, 512],
+  [6, 512, 512]
+]
+
+// This version draws two rectangles and two ellipses.
+// The rectangles are 960x720 and centered at 512,512.
+function drawGrid(p5, x1, x2, y1, y2, z, zoom) {
+  // debug - show border
+  // p5.noFill();
+  // p5.stroke(255, 0, 0)
+  // p5.rect(0, 0, 255, 255);
+}
